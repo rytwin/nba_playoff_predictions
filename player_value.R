bc686fed5bfd09ad9fe45017f6f4bdbe27d7d12e
Ryan Drost <ryandrost@Ryans-MacBook-Pro.local>
2024-04-20 20:28:50 -0400

    finished draft scraping, created player stats file with partial seasons removed

---
+++ player_value.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+data <- read.csv("data/all_player_stats_1980-2024_single_rows.csv")
+
+
+
+
--- scrape_bball_ref_player_data.R
+++ scrape_bball_ref_player_data.R
@@ -11,8 +11,9 @@ player_total_rs <- scrape1 %>%
   filter(Rk != "Rk") %>%
   select(-Rk) %>%
   mutate(across(c(Age, G:year), as.numeric),
-         Player = gsub("\\*", "", Player))
-names(player_total_rs) <- colnames_player_total
+         Player = gsub("\\*", "", Player),
+         tm_order = row_number())
+names(player_total_rs) <- c(colnames_player_total, "tm_order")
 
 
 scrape2 <- scrape_yearly_tables("https://www.basketball-reference.com/leagues/NBA_", seasons, 1, "_per_poss.html")
@@ -301,13 +302,16 @@ awards_df <- mvp_df %>%
   merge(mip_df, by = c("year", "player", "age", "team"), all = TRUE) %>%
   merge(clutch_df, by = c("year", "player", "age", "team"), all = TRUE) %>%
   merge(allnba_df, by = c("year", "player", "age", "team"), all = TRUE) %>%
-  merge(alldef_df, by = c("year", "player", "age", "team"), all = TRUE)
+  merge(alldef_df, by = c("year", "player", "age", "team"), all = TRUE) %>%
+  mutate(across(c(year, age, mvp_rank:alldef_share), as.numeric))
   
 
 all_player_stats <- all_player_stats %>%
   merge(awards_df, by = c("year", "player", "age"), all = TRUE) %>%
+  mutate(tm_order = ifelse(is.na(tm_order), 0, tm_order)) %>%
   rename(team = team.x) %>%
-  select(-team.y)
+  select(-team.y) %>%
+  select(year:g, gs, mp, fg:alldef_share)
 
 write.csv(all_player_stats, "data/all_player_stats_1980-2023.csv", row.names = FALSE)
 
@@ -322,6 +326,204 @@ draft_df <- draft %>%
          player = Player,
          team = Tm,
          college = College,
-         experience = Yrs)
+         experience = Yrs) %>%
+  mutate(across(c(year, pick, experience), as.numeric))
 
 write.csv(draft_df, "data/draft_1960-2023.csv", row.names = FALSE)
+
+draft_df <- draft_df %>%
+  filter(experience != "") %>%
+  rename(draft_year = year) %>%
+  select(-team)
+
+final_pick <- draft_df %>%
+  group_by(player, college, experience) %>%
+  summarize(draft_year = max(draft_year))
+
+draft_single <- draft_df %>%
+  merge(final_pick, by = c("player", "draft_year", "college", "experience"), all.y = TRUE) %>%
+  filter(!(player == "Manute Bol" & college == ""))
+
+
+split_rs_po <- all_player_stats %>%
+  filter((player == "Scott Machado" & year == 2013) | (player == "Shaquille Harrison" & year == 2023)) %>%
+  group_by(year, player, age, pos) %>%
+  summarize(across(c(g:alldef_share), ~ sum(., na.rm = TRUE))) %>%
+  mutate(team = "TOT",
+         tm_order = 0) %>%
+  select(year:age, team, everything())
+
+final_output <- all_player_stats %>%
+  rbind(split_rs_po) %>%
+  rename_teams_abbrev() %>%
+  merge(draft_single, by = "player", all.x = TRUE) %>%
+  mutate(draft_year = ifelse(is.na(draft_year), 0, draft_year),
+         tm_order = case_when(player == "Scott Machado" & year == 2013 & team == "GSW" ~ 16247,
+                              player == "Shaquille Harrison" & year == 2023 & team == "LAL" ~ 22722,
+                              TRUE ~ tm_order)) %>%
+  filter(draft_year < year,
+         !(player == "Bobby Jones" & year >= 2007 & draft_year < 2006),
+         !(player == "Cedric Henderson" & year >= 1998 & draft_year < 1997),
+         !(player == "Dee Brown" & year >= 2007 & draft_year < 2006),
+         !(player == "Gerald Henderson" & year >= 2010 & draft_year < 2009),
+         !(player == "Jeff Taylor" & year >= 2013 & draft_year < 2012),
+         !(player == "Johnny Davis" & year >= 2023 & draft_year < 2022),
+         !(player == "Ken Johnson" & year >= 2003 & draft_year < 2001),
+         !(player == "Larry Johnson" & year >= 1992 & draft_year < 1991),
+         !(player == "Luke Jackson" & year >= 2005 & draft_year < 2004),
+         !(player == "Mark Davis" & year >= 1996 & draft_year < 1995),
+         !(player == "Mike Dunleavy" & year >= 2003 & draft_year < 2002),
+         !(player == "Patrick Ewing" & year >= 2011 & draft_year < 2008),
+         !(player == "Sam Williams" & year >= 1982 & draft_year < 1981),
+         !(player == "George Johnson" & age > 30 & draft_year >= 1978),
+         !(player == "George Johnson" & age < 30 & draft_year < 1978),
+         !(player == "George Johnson" & college == "Stephen F. Austin"),
+         !(player == "Marcus Williams" & college == "UConn" & pos != "PG"),
+         !(player == "Marcus Williams" & college == "Arizona" & pos == "PG"),
+         !(player == "Charles Jones" & year >= 1999 & college == "Louisville"),
+         !(player == "Charles Jones" & year < 1999 & college == "Albany State University" & team == "POR"),
+         !(player == "Charles Jones" & year < 1999 & college == "Albany State University" & team == "PHO"),
+         !(player == "Charles Jones" & year < 1999 & college == "Louisville" & team == "PHI"),
+         !(player == "Charles Jones" & year < 1999 & college == "Louisville" & team == "TOT"),
+         !(player == "Charles Jones" & year < 1999 & college == "Louisville" & team == "CHI"),
+         !(player == "Charles Jones" & year < 1999 & college == "Louisville" & team == "DET"),
+         !(player == "Charles Jones" & year < 1999 & college == "Louisville" & team == "HOU"),
+         !(player == "Charles Jones" & year < 1985 & college == "Louisville"),
+         !(player == "Charles Jones" & year > 1989 & college == "Louisville"),
+         !(player == "Charles Jones" & year == 1989 & college == "Albany State University" & g == 43),
+         !(player == "Charles Jones" & year == 1989 & college == "Louisville" & g == 53),
+         !(player == "Charles Jones" & year < 1989 & team == "WAS" & college == "Louisville"),
+         !(player == "Michael Smith" & year > 1995 & college == "BYU"),
+         !(player == "Michael Smith" & year == 1995 & college == "BYU" & g == 82),
+         !(player == "Michael Smith" & year == 1995 & college == "Providence" & g == 29),
+         !(player == "Eddie Johnson" & college == "Auburn" & pos == "SF"),
+         !(player == "Eddie Johnson" & college == "Auburn" & year > 1987),
+         !(player == "Eddie Johnson" & college == "Illinois" & pos == "SG" & year != 1991),
+         !(player == "Charles Smith" & college != "New Mexico" & year >= 1998)) %>%
+  mutate(college = case_when(player == "Charles Jones" & year >= 1999 & year <= 2000 ~ "Long Island University",
+                             player == "Charles Smith" & year < 1998 & pos == "PG" ~ "Georgetown",
+                             player == "Walker Russell" & year > 1988 ~ "Jacksonville State University",
+                             player == "Mark Jones" & year > 2000 ~ "Central Florida",
+                             player == "Reggie Williams" & year > 2000 ~ "Virginia Military Institute",
+                             player == "Chris Smith" & year > 2000 ~ "Louisville",
+                             player == "Brandon Williams" & year < 2010 ~ "Davidson",
+                             player == "Brandon Williams" & year >= 2022 ~ "Arizona",
+                             player == "Chris Johnson" & year < 2013 ~ "LSU",
+                             player == "Chris Johnson" & year > 2013 ~ "Dayton",
+                             player == "Chris Johnson" & year == 2013 & age == 27 ~ "LSU",
+                             player == "Chris Johnson" & year == 2013 & age == 22 ~ "Dayton",
+                             player == "Chris Wright" & year == 2013 ~ "Georgetown",
+                             player == "Chris Wright" & year < 2015 ~ "Dayton",
+                             player == "Mike James" & year < 2015 ~ "Duquesne",
+                             player == "Mike James" & year < 2024 ~ "Lamar",
+                             TRUE ~ college),
+         draft_year = case_when(player == "Charles Jones" & year >= 1999 & year <= 2000 ~ NA,
+                                player == "Charles Smith" & year < 1998 & pos == "PG" ~ NA,
+                                player == "Walker Russell" & year > 1988 ~ NA,
+                                player == "Mark Jones" & year > 2000 ~ NA,
+                                player == "Reggie Williams" & year > 2000 ~ NA,
+                                player == "Chris Smith" & year > 2000 ~ NA,
+                                TRUE ~ draft_year),
+         pick = case_when(player == "Charles Jones" & year >= 1999 & year <= 2000 ~ NA,
+                          player == "Charles Smith" & year < 1998 & pos == "PG" ~ NA,
+                          player == "Walker Russell" & year > 1988 ~ NA,
+                          player == "Mark Jones" & year > 2000 ~ NA,
+                          player == "Reggie Williams" & year > 2000 ~ NA,
+                          player == "Chris Smith" & year > 2000 ~ NA,
+                          TRUE ~ pick),
+         experience = case_when(player == "Charles Jones" & year >= 1999 & year <= 2000 ~ 2,
+                                player == "Charles Smith" & year < 1998 & pos == "PG" ~ 3,
+                                player == "Walker Russell" & year > 1988 ~ 1,
+                                player == "Mark Jones" & year > 2000 ~ 1,
+                                player == "Reggie Williams" & year > 2000 ~ 7,
+                                player == "Chris Smith" & year > 2000 ~ 1,
+                                TRUE ~ experience),
+         draft_year = ifelse(draft_year == 0, NA, draft_year)) %>%
+  select(player, college, draft_year, pick, year:alldef_share) %>%
+  arrange(player, college, draft_year, pick, year, tm_order)
+
+check <- final_output %>%
+  group_by(player, year, team) %>%
+  summarize(count = n()) %>%
+  filter(count > 1)
+duplicates <- unique(check$player)
+dup_check <- draft_single %>%
+  filter(player %in% duplicates)
+
+check2 <- final_output %>%
+  count(player, college, draft_year, pick, year) %>%
+  count(player, college, draft_year, pick) %>%
+  merge(draft_single, by = c("player", "college", "draft_year", "pick"), all.x = TRUE) %>%
+  filter(experience != n)
+
+check3 <- final_output %>%
+  filter(is.na(pick))
+
+check4 <- final_output %>%
+  filter(is.na(g))
+
+
+write.csv(final_output, "data/all_player_stats_1980-2024.csv", row.names = FALSE)
+
+
+single_rows <- final_output %>%
+  mutate(player_id = paste0(gsub("\\s", "", tolower(player)), "_", gsub("\\s", "", tolower(college)), "_", draft_year, "_", pick),
+         pl_yr_id = paste0(gsub("\\s", "", tolower(player)), "_", gsub("\\s", "", tolower(college)), "_", draft_year, "_", pick, "_", year)) %>%
+  group_by(pl_yr_id) %>%
+  mutate(team = paste(team, collapse = "-"),
+         team = gsub("TOT-", "", team)) %>%
+  distinct(pl_yr_id, .keep_all = TRUE) %>%
+  group_by(player_id) %>%
+  mutate(exp = row_number(),
+         exp = case_when(player_id == "adriandantley_notredame_1976_6" ~ exp + 3,
+                         player_id == "alexenglish_southcarolina_1976_23" ~ exp + 3,
+                         player_id == "bernardking_tennessee_1977_7" ~ exp + 2,
+                         player_id == "braddavis_maryland_1977_15" ~ exp + 2,
+                         player_id == "caldwelljones_albanystateuniversity_1973_32" ~ exp + 6,
+                         player_id == "darryldawkins__1975_5" ~ exp + 4,
+                         player_id == "davecorzine_depaul_1978_18" ~ exp + 1,
+                         player_id == "dennisjohnson_pepperdine_1976_29" ~ exp + 3,
+                         player_id == "gregballard_oregon_1977_4" ~ exp + 2,
+                         player_id == "jacksikma_illinoiswesleyanuniversity_1977_8" ~ exp + 2,
+                         player_id == "jamesedwards_washington_1977_46" ~ exp + 2,
+                         player_id == "jeromewhitehead_marquette_1978_41" ~ exp + 1,
+                         player_id == "johnlong_detroitmercy_1978_29" ~ exp + 1,
+                         player_id == "johnlucas_maryland_1976_1" ~ exp + 3,
+                         player_id == "kareemabdul-jabbar_ucla_1969_1" ~ exp + 10,
+                         player_id == "marquesjohnson_ucla_1977_3" ~ exp + 2,
+                         player_id == "mauricecheeks_westtexasa&muniversity_1978_36" ~ exp + 1,
+                         player_id == "michaelcooper_newmexico_1978_60" ~ exp + 1,
+                         player_id == "mikedunleavy_southcarolina_1976_99" ~ exp + 3,
+                         player_id == "mikemitchell_auburn_1978_15" ~ exp + 1,
+                         player_id == "mosesmalone_NA_NA_NA" ~ exp + 5,
+                         player_id == "mychalthompson_minnesota_1978_1" ~ exp + 1,
+                         player_id == "normnixon_duquesne_1977_22" ~ exp + 2,
+                         player_id == "otisbirdsong_houston_1977_2" ~ exp + 2,
+                         player_id == "purvisshort_jacksonstateuniversity_1978_5" ~ exp + 1,
+                         player_id == "reggietheus_unlv_1978_9" ~ exp + 1,
+                         player_id == "rickeygreen_michigan_1977_16" ~ exp + 2,
+                         player_id == "robertparish_centenary(la)_1976_8" ~ exp + 3,
+                         player_id == "robertreid_st.mary'suniversity_1977_40" ~ exp + 2,
+                         player_id == "t.r.dunn_alabama_1977_41" ~ exp + 2,
+                         player_id == "terrytyler_detroitmercy_1978_23" ~ exp + 1,
+                         player_id == "treerollins_clemson_1977_14" ~ exp + 2,
+                         player_id == "walterdavis_unc_1977_5" ~ exp + 2,
+                         player_id == "waynecooper_neworleans_1978_40" ~ exp + 1,
+                         TRUE ~ exp),
+         yrs_off = ifelse(is.na(lag(year)), 0, year - lag(year) - 1)) %>%
+  select(player:age, exp, yrs_off, team:alldef_share, player_id, pl_yr_id)
+
+# use this to check for players where experience value is not correct
+need_exp <- single_rows %>%
+  group_by(player, player_id) %>%
+  summarize(start_year = min(year),
+            end_year = max(year),
+            draft_year = max(draft_year)) %>%
+  filter(start_year <= 1985,
+         end_year >= 1989,
+         draft_year <= 1979 | is.na(draft_year))
+
+
+write.csv(single_rows, "data/all_player_stats_1980-2024_single_rows.csv", row.names = FALSE)
+         
+         
